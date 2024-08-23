7f454a4ec7f17a9c75e9b20598a67874d5791ab4
Nuru1818 <Myyahoo@1234.>
2024-08-23 16:24:00 +0700

    add role user

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -8,23 +8,32 @@ import { Link } from '@inertiajs/react';
 export default function Authenticated({ user, header, children }) {
     const [showingNavigationDropdown, setShowingNavigationDropdown] = useState(false);
 
-    const menu = [
-        {
-            name: "Dashboard",
-            href: route("dashboard"),
-            current: route().current("dashboard"),
-        },
-        {
-            name: "Users",
-            href: route("users"),
-            current: route().current("users"),
-        },
-        {
-            name: "Absensi",
-            href: route("dashboard"),
-            current: route().current("dashboard"),
-        },
-    ];
+    const menu = (role) => {
+        if(role === "admin") {
+            return [
+                {
+                    name: "Dashboard",
+                    href: route("dashboard"),
+                    current: route().current("dashboard"),
+                },
+                {
+                    name: "Users",
+                    href: route("users"),
+                    current: route().current("users"),
+                },
+                {
+                    name: "Absensi",
+                    href: route("dashboard"),
+                    current: route().current("dashboard"),
+                },
+            ];
+        }else{
+            return [
+                {name: "Dashboard", href: route("dashboard"), current: false},
+            ];
+        }
+    };
+
 
     return (
         <div className="min-h-screen bg-gray-100">
@@ -39,7 +48,7 @@ export default function Authenticated({ user, header, children }) {
                             </div>
 
                             <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                                {menu.map((item, index) => {
+                                {menu(user.role).map((item, index) => {
                                     return(
                                         <NavLink
                                             key={index}
@@ -118,7 +127,7 @@ export default function Authenticated({ user, header, children }) {
 
                 <div className={(showingNavigationDropdown ? 'block' : 'hidden') + ' sm:hidden'}>
                     <div className="pt-2 pb-3 space-y-1">
-                        {menu.map((item, index) => {
+                        {menu(user.role).map((item, index) => {
                             return (
                             <ResponsiveNavLink
                                 key={index}
