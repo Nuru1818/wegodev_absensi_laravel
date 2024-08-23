8aa4bfec4c10a9eb9032afd643939f1e51b83830
Nuru1818 <Myyahoo@1234.>
2024-08-23 16:03:35 +0700

    add create user

--- resources/js/Pages/User/Index.jsx
+++ resources/js/Pages/User/Index.jsx
@@ -1,8 +1,10 @@
 import Pagination from '@/Components/pagination';
+import {Link} from "@inertiajs/react";
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
 
 export default function UserIndex({ auth, users }) {
+
     return (
         <AuthenticatedLayout
             user={auth.user}
@@ -12,8 +14,20 @@ export default function UserIndex({ auth, users }) {
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div className='flex justify-between items-center mb-2'>
+                        <label className='font-bold'>
+                            Total User : {users.total}
+                        </label>
+                        <Link
+                        href={route("users.create")}
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                        >
+                        Create User
+                        </Link>
+                    </div>
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900">
+
                             <table className='min-w-full'>
                                 <thead>
                                     <tr className='border-b-2'>
